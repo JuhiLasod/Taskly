443dde685ce56aaec4fcab6bdb4447feafaf6da5
JuhiLasod <juhilasod29@gmail.com>
2025-05-10 15:19:01 +0530

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from "cors";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+// import routes from "./Routes/authRoutes.js";
+
+const app=express();
+// app.use(cors());
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("API running");
+})
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI)
+
+    .then(()=>{console.log("db connected")})
+    .catch(()=>{console.log("error while connecting to db")});
+    console.log(process.env.MONGO_URI);
+// app.get("/api/auth",routes);
+app.listen(process.env.PORT || 8000,()=>{
+    console.log("server running on",process.env.PORT);
+})
